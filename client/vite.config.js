26d1c8df44112d4a404cd8db1b88b9fc0b9ab877
NageshMane <mane46740@gmail.com>
2025-01-08 09:53:20 +0530

    resolved deployment issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,16 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
  
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,4 +19,21 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+      chunkSizeWarningLimit: 1000, // Suppress warning
+    },
+  },
+});
+
