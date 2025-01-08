1c2f475b801baf4cee0d14c733bc2423c5dbf6cb
NageshMane <mane46740@gmail.com>
2025-01-08 11:12:14 +0530

    Added Deployed Link of Backend

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const _dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://lms-gtai.onrender.com",
     credentials: true
 }));
 
