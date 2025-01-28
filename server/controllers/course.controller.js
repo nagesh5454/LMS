a79dac82b2cb4ff1208211c23f4efc57260a2b42
NageshMane <mane46740@gmail.com>
2025-01-29 00:27:49 +0530

    updated APIs

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -75,6 +75,7 @@ export const searchCourse = async (req,res) => {
 }
 
 
+// API for getPublishedCourse
 export const getPublishedCourse = async (_,res) => {
     try {
         const courses = await Course.find({isPublished:true}).populate({path:"creator", select:"name photoUrl"});
@@ -94,6 +95,8 @@ export const getPublishedCourse = async (_,res) => {
     }
 }
 
+
+// API for getCreatorCourses
 export const getCreatorCourses = async (req,res) => {
     try {
         const userId = req.id;
